c2766a65c30687a7cbc3939fa93b6a7093b65d83
Đỗ Xuân Hướng <84316258+Lazygarde@users.noreply.github.com>
2024-04-14 09:12:29 +0700

    missing file

--- app/src/main/java/dev/ptit/ui/screen/news/NewsDetailsFragment.kt
+++ app/src/main/java/dev/ptit/ui/screen/news/NewsDetailsFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import dagger.hilt.android.AndroidEntryPoint
 import dev.ptit.R
 import dev.ptit.databinding.FragmentNewsDetailsBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class NewsDetailsFragment : Fragment() {
 
     private var _binding: FragmentNewsDetailsBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: NewsViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +31,14 @@ class NewsDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.selectedNews.collect{
+                binding.tvNewsDate.text = it.date
+                binding.tvNewsTitle.text = it.title
+                binding.tvNewsContent.text = it.description
+                Glide.with(this@NewsDetailsFragment).load(it.image).into(binding.ivNewsImage)
+            }
+        }
     }
 
     override fun onDestroyView() {
